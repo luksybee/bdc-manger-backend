37e9718bd56886b3b1dd41b46557b3bc2c5fabdb
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-01-13 17:12:17 +0100

    Update lodgement.js

--- routes/lodgement.js
+++ routes/lodgement.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const lodgementController = require("../controllers/lodgementController");
 
+router.get("/", lodgementController.show);
+router.get("/:id", lodgementController.fetchById);
+
 router.post("/", lodgementController.lodgement);
 router.put("/", lodgementController.lodgementUpdate);
 router.get("/pending", lodgementController.pending);
