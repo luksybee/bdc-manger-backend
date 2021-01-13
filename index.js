84f0a45204bf6ab904f4ea1b5bbcf76d9315d552
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-01-13 07:17:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const http = require("http");
 const logger = require("morgan");
 const path = require("path");
-const webpack = require("webpack");
+// const webpack = require("webpack");
 
 const config = require("./config");
 const passportJWT = require("./middlewares/passportJWT")();
