180cc5ce38ed9657605766a2914008477a18bc3e
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-01-16 17:54:59 +0100

    Update transc_given.js

--- routes/transc_given.js
+++ routes/transc_given.js
@@ -6,9 +6,8 @@ const transc_givenController = require("../controllers/transc_givenController");
 router.get("/", transc_givenController.index);
 
 router.get("/:id", transc_givenController.show);
-router.post("/", transc_givenController.store
-);
-router.patch("/:id", transc_givenController.update);
+router.post("/", transc_givenController.store);
+router.patch("/", transc_givenController.update);
 router.delete("/:id", transc_givenController.delete);
 
 module.exports = router;
