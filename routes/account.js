b12c9c4d7d7b3fa1ffd8ef9bb01f739235bcebc8
Bolajoko Olayinka Luqman <luksyb@gmail.com>
2021-01-17 17:59:55 +0100

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -7,6 +7,5 @@ router.post("/", balanceController.create);
 router.get("/accounts", balanceController.grouped);
 router.get("/", balanceController.accounts);
 router.delete("/:id", balanceController.delete);
-// router.get("/cashier", balanceController.cashier);
 
 module.exports = router;
